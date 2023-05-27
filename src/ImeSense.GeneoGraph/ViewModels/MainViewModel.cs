c4dfea1a0ca3b213e07915a67c61c409a433f135
acidicMercury8 <acidicmercury8@outlook.com>
2023-05-27 19:51:10 +0300

    Add empty Avalonia project

---
+++ src/ImeSense.GeneoGraph/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ImeSense.GeneoGraph.ViewModels;
+
+public class MainViewModel : ObservableObject {
+    public string Greeting => "Welcome to Avalonia!";
+}
