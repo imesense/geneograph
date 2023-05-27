167407c562eaf4e82c4652d975c4e4dd80dcf017
nikitalebedin <nikitalebedin2@gmail.com>
2023-05-27 20:27:25 +0200

    Add basic person model and view

---
+++ src/ImeSense.GeneoGraph/Views/Person.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Views {
+    public partial class Person : UserControl {
+        public Person() {
+            InitializeComponent();
+        }
+    }
+}
