8fe7d7c38407aed841dc9c01c391c8abd517711e
nikitalebedin <nikitalebedin2@gmail.com>
2023-06-13 17:18:05 +0200

    Update presentation layer
    Connect `MainView` to `MainWindow`
    Add `ListBox` for people
    Connect `ListBox` selection with `Properties`

--- src/ImeSense.GeneoGraph/ViewModels/MainViewModel.cs
+++ src/ImeSense.GeneoGraph/ViewModels/MainViewModel.cs
@@ -2,6 +2,16 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ImeSense.GeneoGraph.ViewModels;
 
-public class MainViewModel : ObservableObject {
+public class MainViewModel : ObservableObject 
+{
+    private object _currentViewModel;
+    public object CurrentViewModel {
+        get { return _currentViewModel; }
+        set { _currentViewModel = value; OnPropertyChanged(); }
+    }
+
+    public MainViewModel() {
+        CurrentViewModel = new PeopleViewModel();
+    }
 
 }
