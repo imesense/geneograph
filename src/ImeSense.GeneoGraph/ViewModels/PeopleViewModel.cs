69621ec4d47c55057c7586fb29489ce6440cb629
nikitalebedin <nikitalebedin2@gmail.com>
2023-06-18 22:50:54 +0200

    Add new interface design
    Add person creation function

--- src/ImeSense.GeneoGraph/ViewModels/PeopleViewModel.cs
+++ src/ImeSense.GeneoGraph/ViewModels/PeopleViewModel.cs
@@ -5,21 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Avalonia.Controls;
+
 using CommunityToolkit.Mvvm.ComponentModel;
 
 using ImeSense.GeneoGraph.Models;
+using ImeSense.GeneoGraph.Views;
 
 namespace ImeSense.GeneoGraph.ViewModels {
     public class PeopleViewModel : ObservableObject
     {
         private Person _selectedPerson;
+        private ObservableCollection<Person> _people;
 
-        public ObservableCollection<Person> PeopleList { get; set; }
         public List<string> PeopleSex { get; set; } = new List<string>() {"Male", "Female", "Unknown"};
 
         public PeopleViewModel() 
         {
-            PeopleList = new ObservableCollection<Person>(Person.GetPeople());
+            People = Person.GetPeople();
+
         }
 
         public Person SelectedPerson 
@@ -28,6 +32,19 @@ namespace ImeSense.GeneoGraph.ViewModels {
             set => SetProperty(ref _selectedPerson, value);
         }
 
+        public ObservableCollection<Person> People {
+            get => _people;
+            set => SetProperty(ref _people, value);
+        }
+
+
+        public void AddPersonOpen() 
+        {
+
+            Window AddPersonWindow = new NewPerson();
+            AddPersonWindow.Show();
+
+        }
 
     }
 }
--- src/ImeSense.GeneoGraph/Views/Person.axaml.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls;
-
-namespace ImeSense.GeneoGraph.Views {
-    public partial class Person : UserControl {
-        public Person() {
-            InitializeComponent();
-        }
-    }
-}
