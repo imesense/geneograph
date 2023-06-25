b623e228cc6b6cade923be69290cf69e2e5d1b5d
Renat Shakirov <acidicmercury8@outlook.com>
2023-06-25 13:08:33 +0300

    Integrate dependency injection

--- src/ImeSense.GeneoGraph/App.axaml.cs
+++ src/ImeSense.GeneoGraph/App.axaml.cs
@@ -1,26 +1,42 @@
+using System;
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 
+using Microsoft.Extensions.DependencyInjection;
+
 using ImeSense.GeneoGraph.ViewModels;
 using ImeSense.GeneoGraph.Views;
+using ImeSense.GeneoGraph.Services;
 
 namespace ImeSense.GeneoGraph;
 
 public partial class App : Application {
+    private readonly IServiceProvider _serviceProvider = null!;
+
+    public App() {
+        _serviceProvider = new ServiceCollection()
+            .AddViews()
+            .AddViewModels()
+            .BuildServiceProvider();
+    }
+
     public override void Initialize() =>
         AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted() {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
             // Remove Avalonia data validation
-            ExpressionObserver.DataValidators.RemoveAll(x => x is DataAnnotationsValidationPlugin);
+            ExpressionObserver.DataValidators.RemoveAll(x =>
+                x is DataAnnotationsValidationPlugin);
 
-            desktop.MainWindow = new MainWindow {
-                DataContext = new MainViewModel(),
-            };
+            desktop.MainWindow = _serviceProvider
+                .GetRequiredService<MainWindow>();
+            desktop.MainWindow.DataContext = _serviceProvider
+                .GetRequiredService<MainViewModel>();
         }
 
         base.OnFrameworkInitializationCompleted();
