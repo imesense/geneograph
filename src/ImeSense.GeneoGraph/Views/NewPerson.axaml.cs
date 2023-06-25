58309955ec5f71912bccbc7373bc311f54ef3c8f
nikitalebedin <nikitalebedin2@gmail.com>
2023-06-25 12:23:29 +0300

    Add missing `NewPerson` view and view model

---
+++ src/ImeSense.GeneoGraph/Views/NewPerson.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Views {
+    public partial class NewPerson : Window {
+        public NewPerson() {
+            InitializeComponent();
+        }
+    }
+}
