3e2bad9860d491734d7c8f12f8b52170dd3ea7aa
acidicMercury8 <acidicmercury8@outlook.com>
2024-01-27 23:58:07 +0300

    Add browser project

---
+++ src/ImeSense.GeneoGraph.Browser/Program.cs
@@ -0,0 +1,22 @@
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+
+using Avalonia;
+using Avalonia.Browser;
+using Avalonia.ReactiveUI;
+
+using ImeSense.GeneoGraph;
+
+[assembly: SupportedOSPlatform("browser")]
+
+internal partial class Program
+{
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder.Configure<App>();
+
+    private static async Task Main(string[] args) =>
+        await BuildAvaloniaApp()
+            .WithInterFont()
+            .UseReactiveUI()
+            .StartBrowserAppAsync("out");
+}
