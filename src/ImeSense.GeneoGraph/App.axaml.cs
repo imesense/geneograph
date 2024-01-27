affb600039a4db092ee2616846d13814f505ce98
acidicMercury8 <acidicmercury8@outlook.com>
2024-01-27 23:58:07 +0300

    Add browser project

--- src/ImeSense.GeneoGraph/App.axaml.cs
+++ src/ImeSense.GeneoGraph/App.axaml.cs
@@ -29,6 +29,10 @@ public partial class App : Application {
             desktop.MainWindow = new MainWindow {
                 DataContext = new MainViewModel(),
             };
+        } else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform) {
+            singleViewPlatform.MainView = new MainView {
+                DataContext = new MainViewModel(),
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
