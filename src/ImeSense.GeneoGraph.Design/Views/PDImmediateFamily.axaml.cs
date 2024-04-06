9548a1aa3475be8d8f4efee7f9c67f27e09b3162
nikitalebedin <nikitalebedin2@gmail.com>
2024-04-06 18:19:52 +0200

    Update Person Details view

---
+++ src/ImeSense.GeneoGraph.Design/Views/PDImmediateFamily.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Design.Views {
+    public partial class PDImmediateFamily : UserControl {
+        public PDImmediateFamily() {
+            InitializeComponent();
+        }
+    }
+}
