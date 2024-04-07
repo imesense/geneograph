1112c2389464d75aeb50d79204f23e3fce343ec3
nikitalebedin <nikitalebedin2@gmail.com>
2024-04-07 16:09:09 +0200

    Implement PD tool views

---
+++ src/ImeSense.GeneoGraph.Design/Views/PDPartners.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Design.Views {
+    public partial class PDPartners : UserControl {
+        public PDPartners() {
+            InitializeComponent();
+        }
+    }
+}
