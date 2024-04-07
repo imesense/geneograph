a725493a6fcc8636236833f389aa6d99940235a0
nikitalebedin <nikitalebedin2@gmail.com>
2024-04-07 17:59:29 +0200

    Add albums view

---
+++ src/ImeSense.GeneoGraph.Design/Views/AlbumsView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Design.Views {
+    public partial class AlbumsView : UserControl {
+        public AlbumsView() {
+            InitializeComponent();
+        }
+    }
+}
