03fa96c4ef4bad13bf2f82095b2909ca12e6eefc
acidicMercury8 <acidicmercury8@outlook.com>
2024-04-13 18:12:49 +0300

    Add empty test project

---
+++ test/ImeSense.GeneoGraph.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace ImeSense.GeneoGraph.Tests;
+
+public class Tests {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+}
