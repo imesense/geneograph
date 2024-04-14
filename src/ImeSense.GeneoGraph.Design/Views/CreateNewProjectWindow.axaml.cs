89f8c1e1f3512fc4c6c792f6cb6146d24db08698
nikitalebedin <nikitalebedin2@gmail.com>
2024-04-14 15:28:16 +0200

    Add CreateNewProject Window

---
+++ src/ImeSense.GeneoGraph.Design/Views/CreateNewProjectWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Design.Views {
+    public partial class CreateNewProjectWindow : Window {
+        public CreateNewProjectWindow() {
+            InitializeComponent();
+        }
+    }
+}
