d9b7ee404e1ff34ffff2be6c7abdd264be3526b4
nikitalebedin <nikitalebedin2@gmail.com>
2024-05-24 16:14:22 +0200

    Create add photo settings window

---
+++ src/ImeSense.GeneoGraph.Design/Views/AddPhotoSettingsWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ImeSense.GeneoGraph.Design.Views {
+    public partial class AddPhotoSettingsWindow : Window {
+        public AddPhotoSettingsWindow() {
+            InitializeComponent();
+        }
+    }
+}
